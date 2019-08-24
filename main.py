506e3501673fbe933c765c2a6e475318014aed20
Administrator <admin@example.com>
2019-08-24 20:03:18 +0300

    updated gitignore

--- main.py
+++ main.py
@@ -7,9 +7,4 @@ folder = '/home/elie/Desktop/RayLation/main'
 # Create a new ComplexNumber object
 files = StringChecker(filePrefix, fileEXt, folder)
 
-expressions = files.locateExpressionSemiCol('testAction.php')
-
-# for item in expressions:
-#     print (item[2])
-
-print(expressions)
+print(files)
